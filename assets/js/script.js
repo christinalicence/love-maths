919386dda5adb8bbff3132429ed7884c342cab45
christinalicence <christinalicence@gmail.com>
2025-06-11 08:56:49 +0100

    set up functions structure

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateCorrectAnser () {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuesetion () {
+
+}
+
+function displayMultiplyQuestion () {
+
+}
